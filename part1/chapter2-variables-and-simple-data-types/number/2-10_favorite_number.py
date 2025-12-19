edd6f4669d4975ac5c288ee810df0c7e5f076e49
Joshua <145852592+G4l1le30@users.noreply.github.com>
2025-12-20 02:13:29 +0700

    added number

---
+++ part1/chapter2-variables-and-simple-data-types/number/2-10_favorite_number.py
@@ -0,0 +1,6 @@
+# Use a variable to represent your favorite number. Then using that variable, create a message that reveals your favorite number.
+# Print that message
+
+favorite_number = 14
+message = f"Your favorite number is {favorite_number}"
+print(message)
---
+++ part1/chapter2-variables-and-simple-data-types/number/2-9_number_eight.py
@@ -0,0 +1,7 @@
+# Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print() calls to see the result.
+# You should create four lines.
+
+print(5+3)
+print(10-2)
+print(16/2)
+print(8*1)
