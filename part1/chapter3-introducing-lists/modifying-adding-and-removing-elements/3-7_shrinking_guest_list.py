a4f1081320031a94d124437969028fa1f3d68468
Joshua <145852592+G4l1le30@users.noreply.github.com>
2026-01-12 01:59:08 +0700

    3-7 edited

--- part1/chapter3-introducing-lists/modifying-adding-and-removing-elements/3-7_shrinking_guest_list.py
+++ part1/chapter3-introducing-lists/modifying-adding-and-removing-elements/3-7_shrinking_guest_list.py
@@ -2,8 +2,16 @@
 # and now you have space for only two guests
 
 # Start with your program from exercise 3-6. 
-# Add a new line that prints 
+# Add a new line that prints Add a new line that prints a message saying that you can invite only
+# two people for dinner.
 
+# Use pop() to remove guests from your list one at a time until only two names remain in your list. 
+# Each time you pop a name from your list, 
+# print a message to that person letting them know you’re sorry you can’t invite them to dinner
+
+# Print a message to each of the two people still on your list, letting them know they’re still invited
+
+# Use del to remove the last two names from your list, so you have an empty list
 invitees = [
     'verrel', 'Michael Jackson',
     'ferry irwandi', 'Bahlil', 
