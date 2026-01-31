d70c2fdfdc5604aad1dd445e025c49ab1b82a83e
Joshua <12345678+G4l1le30@users.noreply.github.com>
2026-01-31 17:48:18 +0700

    finalize 4-10

--- part1/chapter4-working-with-lists/3_working-with-part-of-a-list/4-10_slices.py
+++ part1/chapter4-working-with-lists/3_working-with-part-of-a-list/4-10_slices.py
@@ -8,16 +8,21 @@
 # make a list of the multiples of 3, from 3 to 30. Use a for loop to print the numbers in your list
 
 # list comprehension that stores multiples of 3 from 3 to 30
-multiples_of_threes= [i*3 for i in range(3,31)]
+multiples_of_threes = [i * 3 for i in range(3, 31)]
 
 for multiples_of_three in multiples_of_threes:
-        print(multiples_of_three)
+    print(multiples_of_three)
 
-middlePrev = int(len(multiples_of_threes)/2-1) # ambil nilai di tengah list, kurangi satu
-middleNext = int(len(multiples_of_threes)/2+2) # ambil nilai di tengah list, di tambah satu, agar list berhenti menghitung satu angka didepan tengah list
+middlePrev = int(
+    len(multiples_of_threes) / 2 - 1
+)  # ambil nilai di tengah list, kurangi satu
+middleNext = int(
+    len(multiples_of_threes) / 2 + 2
+)  # ambil nilai di tengah list, di tambah satu, agar list berhenti menghitung satu angka didepan tengah list
 
-print(middlePrev, middleNext)
-print(f'The first three items in the list are: {multiples_of_threes[:3]}')
-print(f'Three items from the middle of the list are: {multiples_of_threes[middlePrev:middleNext]}')
-print(f'The last three items in the list are: {multiples_of_threes[-3:]}')
+print(f"The first three items in the list are: {multiples_of_threes[:3]}")
+print(
+    f"Three items from the middle of the list are: {multiples_of_threes[middlePrev:middleNext]}"
+)
+print(f"The last three items in the list are: {multiples_of_threes[-3:]}")
 print(f"Whole list are: {multiples_of_threes}")
