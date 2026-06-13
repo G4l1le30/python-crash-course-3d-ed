9566d12e164f328433d735708bb54c1ef851a9a6
Joshua Washington <145852592+G4l1le30@users.noreply.github.com>
2026-06-13 19:16:54 +0700

    finish chapter 6

--- part1/chapter6-dictionaries/3_nesting/6-12_extensions.py
+++ part1/chapter6-dictionaries/3_nesting/6-12_extensions.py
@@ -3,3 +3,35 @@ We’re now working with examples that are complex enough that they can be exten
 Use one of the example programs from this chapter, and extend it by adding new keys and values,
 changing the context of the program, or improving the formatting of the output.
 """
+
+# from 6-6_polling.py
+
+favorite_languages = {
+    "jen": {
+        "first": "c",
+        "second": "c++",
+        "third": "python",
+    },
+    "sarah": {
+        "first": "rust",
+        "second": "golang",
+        "third": "c#",
+    },
+    "edward": {"first": "python", "second": "java"},
+    "phil": {"first": "lua"},
+}
+
+
+names = ["jen", "phil", "josh", "kimi"]
+
+for name in names:
+    if name in favorite_languages:
+        print(f"Thank you {name}, for responding")
+    else:
+        print(f"{name}, please take our poll!")
+
+for name, favorite_language in favorite_languages.items():
+    s = " is" if len(favorite_language) == 1 else "s are"
+    print(f"{name}'s favorite language{s}: ")
+    for rank, language in favorite_language.items():
+        print(f"{rank}: {language}")
